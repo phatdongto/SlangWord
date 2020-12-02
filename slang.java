6b6ae9c82793d8d3fda4f3a0611b1d86baaee722
phatdongto <phatdongto@gmail.com>
2020-12-02 13:21:30 +0700

    complete first feature

--- slang.java
+++ slang.java
@@ -9,14 +9,23 @@ import java.io.FileReader;
 import java.io.IOException;
 import java.util.logging.Level;
 import java.util.logging.Logger;
+
+import javax.print.event.PrintEvent;
+
+// import sun.swing.PrintColorUIResource;
+
 import java.util.HashMap;
+import java.util.Scanner;
 
 // import java.lang.String;
 
 public class slang {
 
+    public static Scanner keyboard = new Scanner(System.in);
+    public static HashMap<String, String> slangMap = new HashMap<String, String>();
+
     public static HashMap<String, String> GetSlangMap(String url) {
-        HashMap<String, String> slangMap = new HashMap<String, String>();
+        
         try{
             File file = new File(url);
             FileReader fr = new FileReader(file);
@@ -55,8 +64,59 @@ public class slang {
         return slangMap;
     }
 
+    public static String Find_with_slang(String key) {
+        return slangMap.get(key);
+    }
+    
+    public static void GUI(){
+        String out = "";
+        while(!"e".equals(out)){
+            // System.out.print("\033[H\033[2J");
+            // System.out.flush();
+            System.out.println("1. Slang word");
+            // System.out.println("");
+            // System.out.println("");
+            // System.out.println("");
+            // System.out.println("");
+            // System.out.println("");
+            System.out.print("Enter your choose: ");
+            int choose = keyboard.nextInt();
+            String c = keyboard.nextLine();
+            switch (choose) {
+                case 1:
+                System.out.println("Your slang word: ");
+                String key = keyboard.nextLine();
+                // c = keyboard.nextLine();
+                String value = Find_with_slang(key);
+                System.out.println(key + "'s definition is: " + value);
+                break;
+                case 2:
+                System.out.println("Tuesday");
+                break;
+                case 3:
+                System.out.println("Wednesday");
+                break;
+                case 4:
+                System.out.println("Thursday");
+                break;
+                case 5:
+                System.out.println("Friday");
+                break;
+                case 6:
+                System.out.println("Saturday");
+                break;
+                case 7:
+                System.out.println("Sunday");
+                break;
+            }
+            System.out.println("\n\nPress 'Enter' to continue or type 'e' to exit");
+            out = keyboard.next();
+        }
+    }
+
     public static void main(String[] args) {
-        HashMap<String, String> slangMap = GetSlangMap("slang.txt");
-        System.out.println("The collection is: " + slangMap.values());
+        slangMap = GetSlangMap("slang.txt");
+        GUI();
     }
 }
+
